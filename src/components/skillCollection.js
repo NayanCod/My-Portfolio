1f029c6453f5421eb3f4b53cf5e791ab3c8f830a
NayanCod <kumarnayab6@gmail.com>
2024-12-27 20:32:19 +0530

    added skills

--- src/components/skillCollection.js
+++ src/components/skillCollection.js
@@ -1,104 +1,89 @@
 const skills = [
     {
         id: 1,
-        name: "HTML",
-        url: "./skillImages/html.png"
-    },
-    {
-        id: 2,
-        name: "CSS",
-        url: "./skillImages/css.png"
-    },
-    {
-        id: 3,
         name: "Javascript",
         url: "./skillImages/js.png"
     },
     {
-        id: 4,
-        name: "React",
-        url: "./skillImages/react.png"
-    },
-    {
-        id: 5,
-        name: "RTK",
-        url: "./skillImages/rtk.png"
-    },
-    {
-        id: 6,
-        name: "Node.js",
-        url: "./skillImages/node.png"
-    },
-    {
-        id: 7,
-        name: "Figma",
-        url: "./skillImages/figma.png"
-    },
-    {
-        id: 8,
-        name: "MUI",
-        url: "./skillImages/materialui.svg"
-    },
-    {
-        id: 9,
-        name: "Tailwind",
-        url: "./skillImages/tailwind.svg"
-    },
-    {
-        id: 10,
-        name: "C++",
-        url: "./skillImages/cpp.png"
-    },
-    {
-        id: 11,
-        name: "Python",
-        url: "./skillImages/python.png"
-    },
-    {
-        id: 12,
-        name: "Express.js",
-        url: "./skillImages/express.png"
-    },
-    {
-        id: 13,
-        name: "Bootstrap",
-        url: "./skillImages/bootstrap.png"
-    },
-    {
-        id: 14,
-        name: "GSAP",
-        url: "./skillImages/gsap.png"
-    },
-    {
-        id: 15,
-        name: "Git",
-        url: "./skillImages/git.png"
-    },
-    {
-        id: 16,
-        name: "Appwrite",
-        url: "./skillImages/appwrite.png"
-    },
-    {
-        id: 17,
-        name: "Wordpress",
-        url: "./skillImages/wordpress.png"
-    },
-    {
-        id: 18,
-        name: "Arduino",
-        url: "./skillImages/arduino.png"
-    },
-    {
-        id: 19,
+        id: 2,
         name: "Typescript",
         url: "./skillImages/typescript.png"
     },
     {
-        id: 20,
-        name: "RPi",
-        url: "./skillImages/raspberrypi.png"
-    }
+        id: 3,
+        name: "React",
+        url: "./skillImages/react.png"
+    },
+    {
+        id: 4,
+        name: "Vue",
+        url: "./skillImages/vue.png"
+    },
+    {
+        id: 5,
+        name: "Tailwind",
+        url: "./skillImages/tailwind.svg"
+    },
+    {
+        id: 6,
+        name: "Redux",
+        url: "./skillImages/rtk.png"
+    },
+    {
+        id: 7,
+        name: "Bootstrap",
+        url: "./skillImages/bootstrap.png"
+    },
+    {
+        id: 8,
+        name: "Node.js",
+        url: "./skillImages/node.png"
+    },
+    {
+        id: 9,
+        name: "Express.js",
+        url: "./skillImages/express.png"
+    },
+    {
+        id: 10,
+        name: "MongoDB",
+        url: "./skillImages/mongodb.png"
+    },
+    {
+        id: 11,
+        name: "PostgreSQL",
+        url: "./skillImages/postgresql.png"
+    },
+    {
+        id: 12,
+        name: "Prisma",
+        url: "./skillImages/prisma.png"
+    },
+    {
+        id: 13,
+        name: "Fastify",
+        url: "./skillImages/fastify.png"
+    },
+    {
+        id: 14,
+        name: "Git",
+        url: "./skillImages/git.png"
+    },
+    {
+        id: 15,
+        name: "Github",
+        url: "./skillImages/github.png"
+    },
+    {
+        id: 16,
+        name: "Python",
+        url: "./skillImages/python.png"
+    },
+    {
+        id: 17,
+        name: "C++",
+        url: "./skillImages/cpp.png"
+    },
 ];
 
 export default skills;
