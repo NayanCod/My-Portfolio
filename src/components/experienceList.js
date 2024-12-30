343f669e50c7162a5c1559c11de3f6673f11d3df
NayanCod <kumarnayab6@gmail.com>
2024-12-30 10:30:59 +0530

    added experiences

--- src/components/experienceList.js
+++ src/components/experienceList.js
@@ -3,25 +3,25 @@
 const experience  = [
     {
         id:1,
-        role: "Front-End Developer Intern",
-        duration: "Nov, 2024 — Present",
+        role: "Frontend Developer Intern",
+        duration: "Dec, 2024 — Present",
         location:"New Delhi, India",
-        companyName: "Sudoyantra India",
+        companyName: "Pinak Venture",
         webURL: "https://sudoyantra.com",
         desc: "Working as a Frontend Developer, contributing to the development of a responsive, user-friendly website using TypeScript, Tailwind CSS, and HTML. Focused on ensuring cross-browser compatibility and writing scalable, maintainable front-end code.",
         url: "/workedlogos/sudoyantralogo.png",
-        tech: ["Typescript", "Javascrit", "Tailwind"]
+        tech: ["ReactJS", "Redux-toolkit", "React Bootstrap"]
     },
     {
         id:2,
-        role: "Student Co-Ordinator",
-        duration: "Mar, 2022 — May, 2024",
+        role: "Software Developer Intern",
+        duration: "Aug, 2024 — Oct, 2024",
         location:"New Delhi, India",
-        companyName: "Robotics Club, DDUC",
+        companyName: "Sudoyantra India",
         webURL: "https://www.linkedin.com/company/robotics-club-dduc-new",
         desc: "Led the development of event websites and IoT projects involving Raspberry Pi and Arduino, ensuring seamless functionality and team collaboration. Optimized processes, mentored team members, and streamlined vendor integrations to enhance project efficiency.",
         url: "/workedlogos/robotics_club_dduc_new_logo.jpeg",
-        tech: ["Web Development", "Robotics", "Arduino", "Raspi"]
+        tech: ["Vue3", "Tailwind", "Postgresql", "MongoDB"]
     }
 ]
 
